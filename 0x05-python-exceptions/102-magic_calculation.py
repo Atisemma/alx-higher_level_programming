a13cf73989008881a4c94306c0a52442156cc631
Atisemma <atisemma26@gmail.com>
2023-12-20 06:05:57 -0800

    Write the Python function def magic_calculation(a, b):

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/pytho3
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Expectation('Too far')
+            result += a ** b / i
+        except Exception as e:
+            result = b + a
+            break
+        return result
