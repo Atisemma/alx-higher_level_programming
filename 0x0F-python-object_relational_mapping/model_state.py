e53628da4ee3c73ff5400c38ca7a199766a84bea
Atisemma <atisemma26@gmail.com>
2024-03-31 15:53:19 -0700

    Model_state.py added

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""Start link class to table in database
-"""
-import sys
-from model_state import Base, State
+"""takes in the name of a state as an
+argument and lists all cities of that state,
+using the database hbtn_0e_4_usa"""
 
-from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
 
-if __name__ == "__main__":
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-        ),
-        pool_pre_ping=True
-    )
-    Base.metadata.create_all(engine)
+meta = MetaData()
+Base = declarative_base(metadata=meta)
+
+
+class State(Base):
+    """state class"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(120), nullable=False)
