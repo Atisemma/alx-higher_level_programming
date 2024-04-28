d30bdbe6c3ffca0bfe1bedaaca3cca16b5cabf2c
Atisemma <atisemma26'gmail.com>
2024-04-28 13:49:35 -0700

    Write a script that prints all characters of a Star Wars movie

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,21 +1,50 @@
 #!/usr/bin/node
 
-const req = require('request');
-const id = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/';
-req.get(url + id, function (error, res, body) {
+const request = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(apiUrl, (error, response, body) => {
   if (error) {
-    console.log(error);
+    console.error('Error:', error);
+    return;
   }
-  const data = JSON.parse(body);
-  const dd = data.characters;
-  for (const i of dd) {
-    req.get(i, function (error, res, body1) {
-      if (error) {
-        console.log(error);
-      }
-      const data1 = JSON.parse(body1);
-      console.log(data1.name);
+
+  if (response.statusCode !== 200) {
+    console.error('Failed to fetch data:', response.statusCode);
+    return;
+  }
+
+  const movie = JSON.parse(body);
+  const charactersUrls = movie.characters;
+
+  const charactersPromises = charactersUrls.map(characterUrl => {
+    return new Promise((resolve, reject) => {
+      request(characterUrl, (error, response, body) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+
+        if (response.statusCode !== 200) {
+          reject(`Failed to fetch data for character ${characterUrl}: ${response.statusCode}`);
+          return;
+        }
+
+        const character = JSON.parse(body);
+        resolve(character.name);
+      });
     });
-  }
+  });
+
+  Promise.all(charactersPromises)
+    .then(characters => {
+      characters.forEach(character => {
+        console.log(character);
+      });
+    })
+    .catch(error => {
+      console.error(error);
+    });
 });
